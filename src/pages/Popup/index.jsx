06a5f3504522c9dbf0f7e7eaa3b16bea607ef702
Krish <kpkapadia@enacton.com>
2022-05-31 13:48:44 +0530

    refact

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Popup from './Popup';
-import './index.css';
+import Popup from './components/Popup';
+import './css/index.css';
 import '../../tailwind/output.css';
 
 render(<Popup />, window.document.querySelector('#app-container'));
