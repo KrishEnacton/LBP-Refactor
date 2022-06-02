5c98f3fd63d3f184740666d40ebb76fc20c47a4a
Krish <kpkapadia@enacton.com>
2022-06-02 14:34:42 +0530

    feat: i18next implemented

--- src/pages/Popup/components/screens/Main.jsx
+++ src/pages/Popup/components/screens/Main.jsx
@@ -1,21 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { getUserDashboard } from '../../../../common/dataProvider';
-import { translate } from '../../../../common/utils_global';
+import { set_user_lang, translate } from '../../../../common/utils_global';
 import { UserContext } from '../../context/UserContext';
 import Home from './Home';
 import Profile from './Profile';
+import ReferNEarn from './ReferNEarn';
 
 const Main = () => {
   const [loading, setLoading] = useState(false);
   const [userData, setUserData] = useState(false);
-  const [lang, setLang] = useState();
 
   useEffect(() => {
+    set_user_lang();
     setLoading(true);
     getUserDashboard().then((data) => {
-      chrome.storage.local.get((res) => {
-        setLang(res.lang);
-      });
       setUserData(data);
       setLoading(false);
     });
@@ -24,9 +22,10 @@ const Main = () => {
   return (
     <UserContext.Provider value={[userData, setUserData]}>
       <div>
-        {loading ? translate('loading') : translate('data')}: {lang}
+        {loading ? translate('loading') : translate('data')}
         <Home />
         <Profile />
+        <ReferNEarn />
       </div>
     </UserContext.Provider>
   );
